e4e5462dcbef2db06091430be45be030a53b94ee
Florian <florian.drevet@epita.fr>
2026-04-21 16:00:25 +0200

    Ajout de la migration pour la gestion des cadeaux et mise à jour du modèle de données

---
+++ src/back/Mariage.Infrastructure/Migrations/20260421140012_Gifts.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Mariage.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Gifts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
